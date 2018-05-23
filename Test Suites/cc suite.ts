98c123c5a32994478147e299f275a8710b05f7b9
jccf <jccf@windows10.microdone.cn>
2018-05-23 19:55:06 +0800

    全部

---
+++ Test Suites/cc suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>cc suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>53acff0a-f753-4891-8989-fff37197bce2</testSuiteGuid>
+   <testCaseLink>
+      <guid>1bea9f31-d9dc-40f5-876c-045d071b659b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/cc</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
